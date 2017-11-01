e567de8e2a79dbf4bb5fa6832459ae4a85beea0d
HatsanK <hatsankun@gmail.com>
2017-11-01 17:14:11 +0900

    1.2.2m1

--- buildtool.sh
+++ buildtool.sh
@@ -21,9 +21,21 @@ set +v
 echo "* Firefox Buildtool: Creating package..."
 pushd $DES/ > /dev/null
 # zip ../krpatch.cinderella.girls.xpi -qr *
-# web-ext lint
-web-ext sign
-#mv web-ext-artifacts/*.xpi ../cg_krpatch.cinderella.girls.xpi
+
+# https://github.com/mozilla/web-ext/issues/793
+unset WEB_EXT_API_KEY
+unset WEB_EXT_API_SECRET
+
+web-ext lint -v
+
+# https://github.com/mozilla/web-ext/issues/793
+export WEB_EXT_API_KEY=$ENV_WEB_EXT_API_KEY
+export WEB_EXT_API_SECRET=$ENV_WEB_EXT_API_SECRET
+
+web-ext sign -v
+
+zsh -c 'autoload zmv; zmv "web-ext-artifacts/_-(*)-an+fx.xpi" "../cg-krpatch-$1-an+fx.xpi"'
+
 popd > /dev/null
 
 echo "* Firefox Buildtool: Done."
