39ecb0b8d51111cb2965dd775790fc418dfc9d0b
you1914 <33338410+you1914@users.noreply.github.com>
2017-11-22 22:17:49 +0900

    1.3.0
    이벤트 3개(바라, pmf, 로얄) 추가
    UI 한글화 관리 패널 추가

---
+++ js/popup.js
@@ -0,0 +1,47 @@
+function disable_css ()
+{
+	chrome.storage.sync.set({ 'disable_css': true });
+}
+
+
+function enable_css ()
+{
+	chrome.storage.sync.remove('disable_css');
+}
+
+function update_buttons_css ()
+{
+	chrome.storage.sync.get('disable_css', function (items)
+	{
+		var button_css_disable = document.getElementById('button-css-disable');
+		var button_css_enable  = document.getElementById('button-css-enable');
+
+		if (items.disable_css === true)
+		{
+			button_css_disable.className = button_css_disable.className + ' active';
+			button_css_enable.className = button_css_enable.className.replace(/active/g, '');
+		}
+		else
+		{
+			button_css_enable.className = button_css_enable.className + ' active';
+			button_css_disable.className = button_css_disable.className.replace(/active/g, '');
+		}
+	});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('button-css-disable').addEventListener('click', function()
+	{
+		disable_css();
+		update_buttons_css();
+	});
+
+	document.getElementById('button-css-enable').addEventListener('click', function()
+	{
+		enable_css();
+		update_buttons_css();
+
+	});
+
+	update_buttons_css();
+});
