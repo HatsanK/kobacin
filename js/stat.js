7b0cf3f337eeed35d3f2c056a905e09cf7e5198b
you1914 <33338410+you1914@users.noreply.github.com>
2017-11-23 18:28:23 +0900

    1.3.1
    공코모드시 스테미너가 찍히는 버그를 수정하였습니다.

--- js/stat.js
+++ js/stat.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
-	     chrome.storage.sync.get('disable_css', function (items){
-			if (items.disable_css === true){
-				var link = document.createElement('link');
-				link.setAttribute("rel", "stylesheet");
-				link.setAttribute("type", "text/css");
-				link.setAttribute("href", chrome.extension.getURL("../css/attk.css"));
-				document.getElementsByTagName("head")[0].appendChild(link);
+		chrome.storage.sync.get('disable_ui', function (items){
+			if (items.disable_ui === true){
+				 
 			} else {
-				var link = document.createElement('link');
-				link.setAttribute("rel", "stylesheet");
-				link.setAttribute("type", "text/css");
-				link.setAttribute("href", chrome.extension.getURL("../css/stam.css"));
-				document.getElementsByTagName("head")[0].appendChild(link);
+				stat();
 			}
-		});
+	});
 });
 
+function stat(){
+	chrome.storage.sync.get('disable_css', function (items){
+	if (items.disable_css === true){
+		var link = document.createElement('link');
+		link.setAttribute("rel", "stylesheet");
+		link.setAttribute("type", "text/css");
+		link.setAttribute("href", chrome.extension.getURL("../css/attk.css"));
+		document.getElementsByTagName("head")[0].appendChild(link);
+	} else {
+		var link = document.createElement('link');
+		link.setAttribute("rel", "stylesheet");
+		link.setAttribute("type", "text/css");
+		link.setAttribute("href", chrome.extension.getURL("../css/stam.css"));
+		document.getElementsByTagName("head")[0].appendChild(link);
+	}
+	});
+}
 
 
